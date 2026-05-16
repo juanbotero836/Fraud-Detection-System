c1f37b4fe09c14ea1b702175f275c65b318deee6
juanbotero836 <juan.botero836@pascualbravo.edu.co>
2026-05-16 11:35:06 -0500

    feat: add expetiment runner for fraud detection models

---
+++ src/runner.py
@@ -0,0 +1,23 @@
+from features import FeaturePipeline
+from train import FraudDetectionTrainer
+
+DATA_PATH = '../data/raw/creditcard.csv'
+
+def main():
+    # 1. Cragar y preparar los datos
+    feature_pipeline = FeaturePipeline()
+    df = feature_pipeline.load_data(DATA_PATH)
+    X, y = feature_pipeline.split_features_target(df)
+    X_train_raw, X_test_raw, y_train, y_test = feature_pipeline.get_train_test_split(X, y)
+    
+    # 2. Transformar 
+    X_train = feature_pipeline.fit_transform_train(X_train_raw)
+    X_test = feature_pipeline.transform_test(X_test_raw)
+    
+    # 3. Entrenar y loggear los experimentos
+    trainer = FraudDetectionTrainer()
+    trainer.run_all_experiments(X_train, y_train, X_test, y_test)
+    
+if __name__ == '__main__':
+    main()
+    
