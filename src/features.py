a216ff8848566bf05d3ad3319075c124c90f160a
juanbotero836 <juan.botero836@pascualbravo.edu.co>
2026-05-16 10:31:25 -0500

    feat: add sklearn preprocessing pipeline

---
+++ src/features.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+class FeaturePipeline:
+    def __init__(self, test_size: float = 0.2, random_state:int = 42):
+        self.test_size = test_size
+        self.random_state = random_state
+        self.pipeline = self._built_pipeline
+        
+    def _built_pipeline(self) -> Pipeline:
+        """
+        Construye el preprocesamiento de los datos
+        """
+        return Pipeline([
+            ('scaler', StandardScaler())
+        ])
+        
+    def load_data(self, path : str) -> pd.DataFrame:
+        """
+        Cargar el dataset y convertirlo en df
+        """
+        return pd.DataFrame(path)
+        
+    def split_features_target(self, df : pd.DataFrame):
+        X = df.drop(columns=['Class'])
+        y = df['Class']
+        return X, y
+    
+    def get_train_test_split(self, X, y):
+        return train_test_split(
+            X, y,
+            test_size=self.test_size,
+            random_state=self.random_state,
+            stratify=y
+        )
+    def fit_transform_train(self, X_train):
+        return self.pipeline.fit_transform(X_train)
+    
+    def transform_test(self, X_test):
+        """
+        Solo transform en test. El Pipeline ya fue ajustado en train.
+        """
+        return self.pipeline.transform(X_test)
+        
+    
